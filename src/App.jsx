1f3d96bdd471f7333ec392f24403aa6c5d2dc814
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-10-15 01:24:41 +0300

    selecting existing project
    seeing project details and also add deleting projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 import ProjectSidebar from './components/ProjectSidebar';
+import SelectedProject from './components/SelectedProject';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
   });
+
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState((prevState) => {
@@ -44,7 +54,25 @@ function App() {
     });
   }
 
-  let content;
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = (
+    <SelectedProject onDelete={handleDeleteProject} project={selectedProject} />
+  );
 
   if (projectsState.selectedProjectId === null) {
     content = (
@@ -53,10 +81,14 @@ function App() {
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
+  // else {
+  //   content = <SelectedProject project={selectedProject} />;
+  // }
 
   return (
     <main className='h-screen my-8 flex gap-8'>
       <ProjectSidebar
+        onSelect={handleSelectProject}
         projects={projectsState.projects}
         onStartAddProject={handleStartAddProject}
       />
